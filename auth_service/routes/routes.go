02d9f62a78e1a0a3710396ccb350ee659bc223d1
Jonathan1366 <s32220020@student.ubm.ac.id>
2025-05-21 16:39:45 +0700

    feat(auth_service): fixing oauth2 #7

--- auth_service/routes/routes.go
+++ auth_service/routes/routes.go
@@ -6,7 +6,7 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
-func SetupRoutes(app *fiber.App, SellerHandler *handlers.SellerHandler, UserHandler *handlers.UserHandler, GoogleHandler *handlers.GoogleHandler) {
+func SetupRoutes(app *fiber.App, sellerHandlers *handlers.SellerHandler, userHandlers *handlers.UserHandler, googlehandlers *handlers.GoogleHandler) {
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
 			"error":"404 not found",
@@ -19,24 +19,23 @@ func SetupRoutes(app *fiber.App, SellerHandler *handlers.SellerHandler, UserHand
 
 	//SELLER
 	seller:= auth.Group("/seller")
-	seller.Post("/register", Seller.RegisterSeller)
-	seller.Post("/login", Seller.LoginSeller)
-	seller.Post("/otp/send", Seller.SendOTP)
-	seller.Post("/otp/verify", Seller.VerifyOTP) 
-	seller.Post("/logout", Seller.LogoutSeller)
-	seller.Post("/store", Seller.StoreSeller)
+	seller.Post("/register", sellerHandlers.RegisterSeller)
+	seller.Post("/login", sellerHandlers.LoginSeller)
+	seller.Post("/otp/send", sellerHandlers.SendOTP)
+	seller.Post("/otp/verify", sellerHandlers.VerifyOTP) 
+	seller.Post("/logout", sellerHandlers.LogoutSeller)
+	seller.Post("/store", sellerHandlers.StoreSeller)
 
 	//S3 BUCKET
 	// seller.Post("/presignurl", Seller.GeneratePresignedUploadURL)
 	
 	//USER
 	user:= auth.Group("/user")
-	user.Post("/register", User.RegisterUser)
-	user.Post("/login", User.LoginUser)
-	user.Post("/logout", User.LogoutUser)
+	user.Post("/register", userHandlers.RegisterUser)
+	user.Post("/login", userHandlers.LoginUser)
+	user.Post("/logout", userHandlers.LogoutUser)
 
 	//OAUTH2
 	google:= auth.Group("/google")
-	google.Post("/login", google.GoogleSignIn)
-
+	google.Post("/login", googlehandlers.GoogleSignIn)
 }
