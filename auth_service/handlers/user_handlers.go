96255a2af9a2365471ad12255aeb9d9e24bbca27
Jonathan1366 <s32220020@student.ubm.ac.id>
2025-07-16 14:51:54 +0700

    feat(user_handler): update query register login user3

--- auth_service/handlers/user_handlers.go
+++ auth_service/handlers/user_handlers.go
@@ -66,7 +66,7 @@ func (h *UserHandler) RegisterUser(c *fiber.Ctx) error {
 	defer conn.Release()
 
 	// Insert user into the database
-	query := `INSERT INTO user (nama_users, email, password) VALUES ($1, $2, $3, $4)`
+	query := `INSERT INTO user (nama_users, email, password) VALUES ($1, $2, $3)`
 	_, err = conn.Exec(ctx, query, user.NamaUsers, user.Email, user.Password)
 	if err != nil {
 		if strings.Contains(err.Error(), "duplicate key value") {
